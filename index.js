b452fc4de84f0a16f39e0808d5660072f532a78c
Raghavan <66511693+raghavan21@users.noreply.github.com>
2022-01-26 23:47:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+function myFunction() {
+  var x = document.getElementById("canvas3d");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+$(function() {
+  $(this).bind("contextmenu", function(e) {
+    e.preventDefault();
+  });
+});
+
+//disable_inspect_shortcut
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+}
+//l_1-input_verify
 function formValidation(inputText){
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(inputText.value.match(mailformat)){
-    alert("Valid");
-    document.registration.email_1.focus();
-    return true;
+  var ans = "kalilinux";
+  if(inputText.value.match(ans))
+  {
+    alert("Correct");
+    window.location.href = 'https://www.svce.ac.in';
   }
   else{
-    alert("wrong format");
-    document.registration.email_1.focus();
-    return false;
+    alert("Incorrect");
   }
 }
